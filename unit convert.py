6102f33cfdbf8c115ef4d57248b1249c56684e72
Wasaf Jameel <wasafjameel@gmail.com>
2026-09-14 15:15:38 +0500

    first commit

---
+++ unit convert.py
@@ -0,0 +1,10 @@
+f =float(input("enter temperature:"))
+celcius =(f-32) *  5/9
+print("Fahrenheit :", {f})
+c = float(input("enter temperature:"))
+Fahrenheit = (c *9/5) + 532
+print("Celcius:", {c})
+if celcius=="c":
+    print("Result", Fahrenheit)
+elif Fahrenheit=="f":
+    print("Result",celcius )
